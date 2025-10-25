3cf2f729094b82fd55465f2f4690ea2d8a5f3841
iTamojeet <bigcodingdinosaur@gmail.com>
2025-10-25 11:41:33 +0530

    first commit

---
+++ day1-RESTTEMPLATE/deptmicroservice/src/main/java/com/dept/DeptmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptmicroserviceApplication.class, args);
+	}
+
+}
