f077dfb832c33b900f54f2d7e4da7130655a71fb
iTamojeet <bigcodingdinosaur@gmail.com>
2025-11-01 12:55:12 +0530

    webflux

--- day1-RESTTEMPLATE/empmicroservice/src/main/java/com/emp/service/impl/EmployeeServiceImpl.java
+++ day1-RESTTEMPLATE/empmicroservice/src/main/java/com/emp/service/impl/EmployeeServiceImpl.java
@@ -10,13 +10,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+    private WebClient webClient;
+//    private RestTemplate restTemplate;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -59,10 +61,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity
-                ("http://localhost:8888/api/departments/" + employee.getDepartmentCode(),DepartmentDto.class);
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity
+//                ("http://localhost:8888/api/departments/" + employee.getDepartmentCode(),DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
+
+        DepartmentDto departmentDto =
+                webClient.get()
+                        .uri("http://localhost:8888/api/departments/"+employee.getDepartmentCode())
+                        .retrieve()
+                        .bodyToMono(DepartmentDto.class)
+                        .block();
 
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
@@ -79,6 +89,4 @@ public class EmployeeServiceImpl implements EmployeeService {
         return apiResponseDto;
 
     }
-
-
 }
